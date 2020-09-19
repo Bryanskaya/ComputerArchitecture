b4876bbd881956a41d18828501cf4776351f5f23
Bryanskaya <bryanskayakatya@yandex.ru>
2020-09-19 21:34:40 +0300

    ADD: tests

--- lab_02/task_13/index.js
+++ lab_02/task_13/index.js
@@ -1,18 +1,55 @@
 "use strict";
 
-const readlineSync = require('readline-sync');
+function read_file(folder, fileName){
+    const fs = require("fs");
 
-const a = readlineSync.question("Input extention: ");
+    let strings = fs.readFileSync(folder + "/" + fileName, "utf8");
 
-const fs = require("fs");
-
-const folder = "./";
-
-const arr = fs.readdirSync(folder);
-
-console.log("Length: " + arr.length);
-
-for(let i = 0; i < arr.length; i++) {
-    const fileName = arr[i];
-    console.log(fileName);
+    if (strings == ""){
+        console.log("Empty file");
+        console.log();
+        return;
+    }
+    
+    console.log(strings);
+    console.log();
 }
+
+function choose_files(){
+    let found = false;
+
+    const readlineSync = require('readline-sync');
+
+    const ext = readlineSync.question("Input extention: ");
+    const folder = readlineSync.question("Input address: ");
+
+    console.log();
+
+    const fs = require("fs");
+
+    const arr = fs.readdirSync(folder);
+
+    for(let i = 0; i < arr.length; i++) {
+        const fileName = arr[i];
+        let temp_folder = fileName.split('.');
+        if (temp_folder.length != 2)
+            continue;
+        
+        if (temp_folder[1] === ext){
+            console.log(fileName);
+            read_file(folder, fileName);
+            found = true;
+        }
+    }
+
+    if (!found){
+        console.log("NOT found");
+        console.log();
+    }
+}
+
+function main(){
+    choose_files();
+}
+
+main();
